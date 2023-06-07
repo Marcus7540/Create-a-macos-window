62ed61dc53157e030372e331bb4bf18a06ad5d11
Marcus7540 <133431095+Marcus7540@users.noreply.github.com>
2023-06-07 18:41:35 +0200

    Add files via upload

---
+++ First/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First
+//
+//  Created by Marcus Afrem on 2023-06-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by Marcus Afrem on 2023-06-07.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
